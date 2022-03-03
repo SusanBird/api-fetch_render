75af37cd240ee4e060bdbdfdd47b8e5bac40c16d
SusanBird <susanbird391@gmail.com>
2022-03-03 11:45:05 -0800

    grabbed dom elements in app.js

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 // import functions
 
 // grab DOM elements
+const template = document.querySelector('#template');
+// validate with console.log(template); 
+const selectEl = document.querySelector('select');
+// validate with console.log(select); 
+
+const list = document.querySelector('#list');
 
 // set event listeners 
     // get user input
