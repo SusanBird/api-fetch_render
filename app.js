226428ce29db2fb1b61eaf252d45265e364548e8
SusanBird <susanbird391@gmail.com>
2022-03-03 12:10:43 -0800

    pokemon info showing on the page

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ async function loadPokedex() {
     list.classList.add('pokemon');
 
     for (let pokemon of pokedex) {
-        const clone = template.textContent.cloneNode(true);
+        const clone = template.content.cloneNode(true);
 
         const name = clone.querySelector('h2');
         const image = clone.querySelector('img');
@@ -34,6 +34,28 @@ async function loadPokedex() {
 
 
 // set event listeners 
+selectEl.addEventListener('change', async (event) => {
+    const selected = event.target.value;
+    console.log('user click');
+
+
+    if (selected === 'none') {
+        const p = document.createElement('p';)
+
+        p.textContent = 'please select an API';
+
+        errorElement.appendChild(p);
+    }    
+    else if (selected === 'pokemon') {
+        list.innerHTML = '';
+        await loadPokedex();
+    } else if (selected === 'star-wars') {
+       // TODO write loadStarWars function
+        list.innerHTML = '';
+        await loadStarWars();
+    }
+});
+});
     // get user input
     // use user input to update state 
     // update DOM to reflect the new state
